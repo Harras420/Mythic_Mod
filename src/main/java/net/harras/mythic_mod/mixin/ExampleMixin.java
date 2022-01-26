bbec7db7c3c49c0512b4379b88ea2d8f750dcc03
Harras <harryvermeulen21@gmail.com>
2022-01-26 19:21:55 +0200

    Initial commit

---
+++ src/main/java/net/harras/mythic_mod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.harras.mythic_mod.mixin;
+
+import net.harras.mythic_mod.MythicMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		MythicMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
